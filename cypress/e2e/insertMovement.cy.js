2a03692011810ceadc243ab0568e4ce74fcc4a17
Patricie Colman <paty@192.168.1.23>
2024-03-14 20:15:19 -0300

    insertMovements spec

---
+++ cypress/e2e/insertMovement.cy.js
@@ -0,0 +1,28 @@
+describe('Insert movement', () => {
+    context('Positive Scenarios', () => {
+        it('Insert movement', () => {
+            cy.visit('https://barrigareact.wcaquino.me')
+            cy.url().should('eq', 'https://barrigareact.wcaquino.me/login')
+            cy.get('[data-test="email"]').type('patricie.lope@gmail.com')
+            cy.get('input[placeholder="Senha"]').type('Paty123456')
+            cy.contains('button', 'Entrar').click()
+            cy.get('[data-test="menu-settings"]').click()
+            cy.get('.nav-link.dropdown-item').contains("Contas").click()
+            cy.get('[data-test="nome"]').type("Tom")
+            cy.get('.btn').click()
+            cy.get('.toast-close-button').parent().should('have.css', 'background-color', 'rgb(81, 163, 81)')
+            
+            
+
+            
+
+
+
+
+
+
+            // cy.get('[data-test="menu-movimentacao"]').click()
+            
+        })
+    })
+})
